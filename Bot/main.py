5e46f6570a66d41d33c6ad7c677c6ab4f7a43ceb
PetarVinarov <kairo.ask@gmail.com>
2021-10-26 16:41:00 +0300

    I just made my first bot

--- Bot/main.py
+++ Bot/main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import os
+import random
+import asyncio
+import discord
+from discord.ext import commands
+from config import TOKEN
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = commands.Bot(command_prefix='T!',
+                   description='''Calling twitch into yours channel''')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+    await bot.change_presence( status=discord.Status.offline)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_voiceline(champ):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+  voicelines = os.listdir(champ)
+
+  rvoice = random.choice(voicelines)
+  return rvoice
+
+@bot.command()
+async def Twitch(ctx):
+
+    try:
+        channel = ctx.author.voice.channel
+    except Exception:
+        return await ctx.send("You must be in a voice channel")
+    game = discord.Game("I was hiding")
+    await bot.change_presence(activity=game, status=discord.Status.online)
+    vc = await channel.connect()
+    vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=os.path.abspath(f"Twitch/{get_voiceline('Twitch')}")))
+    # Sleep while audio is playing.
+    while vc.is_playing():
+        sleep(.1)
+    await vc.disconnect()
+    await bot.change_presence(status=discord.Status.offline)
+
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(bot.start(TOKEN))
+except KeyboardInterrupt:
+    loop.run_until_complete(bot.logout())
+finally:
+    loop.close()
